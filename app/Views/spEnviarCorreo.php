ac19580cc3f935b835ac9abd0eab596b7bf5d250
AlejoCloquell15 <yutunpremium16@gmail.com>
2023-08-11 11:21:33 -0300

    Guardado

---
+++ app/Views/spEnviarCorreo.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Introduce el mail con el que te has registrado en Disertus, te enviaremos un codigo a tu correo
+        con el cual podras cambiar tu contraseña
+    </p>
+    <form action="<?php echo base_url();?>enviarCorreo" method="post">
+        <input type="text" name="correo">
+        <input type="submit" value="enviarCorreo">
+    </form>
+    <?php 
+    if(isset($mensaje)){
+        echo $mensaje;
+    }
+    ?>
+</body>
+</html>
